c1d4e0de293ddaad2f6645c79fa57459adf7cbfb
Airtune <84182637+Airtune@users.noreply.github.com>
2022-12-03 17:18:33 -0500

    v1.1.6 clean build

--- dist/src/lib/get-banano-account-public-key.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getBananoAccountPublicKey = void 0;
-var bananojs = require("@bananocoin/bananojs");
-var getBananoAccountPublicKey = function (account) {
-    return bananojs.getAccountPublicKey(account);
-};
-exports.getBananoAccountPublicKey = getBananoAccountPublicKey;
-//# sourceMappingURL=get-banano-account-public-key.js.map
--- dist/types/banano.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type TAccount = `ban_${string}`;
-export declare type TBlockHash = string;
-export declare type TPublicKey = string;
